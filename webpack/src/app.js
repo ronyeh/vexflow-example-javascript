9d63b86999ce74945d26ad13e0bf3f3443ce511b
Ron B. Yeh <ronyeh@gmail.com>
2021-12-24 00:06:28 -1000

    Test with new build:
    npm install https://vexflow-demo.surge.sh/4.0.0-beta-2021-12-24.tgz

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -1,10 +1,13 @@
 // Run `npm start`
 
 // Include all three music engraving fonts.
-// import { Vex } from "vexflow"; // 772 KiB
+// import { Vex } from "vexflow"; // 773 KiB
 
 // Include only the Petaluma Music Font
 import { Vex } from "vexflow/petaluma"; // 461 KiB
+
+// Include only the Gonville Music Font
+// import { Vex } from "vexflow/gonville"; // 441 KiB
 
 const factory = new Vex.Flow.Factory({
     renderer: { elementId: "output", width: 500, height: 130 },
